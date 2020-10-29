f1fdaf73aba9a68a11cb82be04c7150869a1253a
Yanone <post@yanone.de>
2020-10-29 11:34:30 +0100

    Just formatting

--- sources/tools/removeUnwantedVFInstances.py
+++ sources/tools/removeUnwantedVFInstances.py
@@ -6,44 +6,49 @@ file = sys.argv[1]
 ttFont = fontTools.ttLib.TTFont(file)
 
 allowed_stylenames = (
-"Thin",
-"ExtraLight",
-"Light",
-"Regular",
-"Medium",
-"SemiBold",
-"Bold",
-"ExtraBold",
-"Black",
-"ExtraBlack",
-"Thin Italic",
-"ExtraLight Italic",
-"Light Italic",
-"Italic",
-"Medium Italic",
-"SemiBold Italic",
-"Bold Italic",
-"ExtraBold Italic",
-"Black Italic",
-"ExtraBlack Italic",
+    "Thin",
+    "ExtraLight",
+    "Light",
+    "Regular",
+    "Medium",
+    "SemiBold",
+    "Bold",
+    "ExtraBold",
+    "Black",
+    "ExtraBlack",
+    "Thin Italic",
+    "ExtraLight Italic",
+    "Light Italic",
+    "Italic",
+    "Medium Italic",
+    "SemiBold Italic",
+    "Bold Italic",
+    "ExtraBold Italic",
+    "Black Italic",
+    "ExtraBlack Italic",
 )
 
 
 def forbidden_instance():
-    for instance in ttFont['fvar'].instances:
-        name = ttFont['name'].getName(
-          instance.subfamilyNameID,
-          PlatformID.WINDOWS,
-          WindowsEncodingID.UNICODE_BMP,
-          WindowsLanguageID.ENGLISH_USA
-        ).toUnicode()
+    for instance in ttFont["fvar"].instances:
+        name = (
+            ttFont["name"]
+            .getName(
+                instance.subfamilyNameID,
+                PlatformID.WINDOWS,
+                WindowsEncodingID.UNICODE_BMP,
+                WindowsLanguageID.ENGLISH_USA,
+            )
+            .toUnicode()
+        )
 
-        if not name in allowed_stylenames:
+        if name not in allowed_stylenames:
             return instance
+
 
 while forbidden_instance():
     instance = forbidden_instance()
-    ttFont['fvar'].instances.remove(instance)
-    print('Deleted', instance)
+    ttFont["fvar"].instances.remove(instance)
+    print("Deleted", instance)
 
 ttFont.save(file)
