2420e6bb7ec519d5f6bff864429f0f82c1f5b142
Yanone <post@yanone.de>
2021-04-29 15:16:10 +0200

    State before moving to a gftools builder based production flow

--- sources/tools/fix-name-table-vf.py
+++ sources/tools/fix-name-table-vf.py
@@ -7,16 +7,22 @@ font_path = "../fonts/Variable/Georama[wdth,wght].ttf"
 print(f"Treating {font_path}")
 ttfont = TTFont(font_path)
 ttfont["name"].setName(
-    string="Regular", nameID=17, platformID=3, platEncID=1, langID=0x409
+    string="ExtraCondensed", nameID=16, platformID=3, platEncID=1, langID=0x409
 )
-ttfont["OS/2"].usWeightClass = 400
+ttfont["name"].setName(
+    string="Thin", nameID=17, platformID=3, platEncID=1, langID=0x409
+)
+ttfont["OS/2"].usWeightClass = 100
 ttfont.save(font_path)
 
 font_path = "../fonts/Variable/Georama-Italic[wdth,wght].ttf"
 print(f"Treating {font_path}")
 ttfont = TTFont(font_path)
 ttfont["name"].setName(
-    string="Italic", nameID=17, platformID=3, platEncID=1, langID=0x409
+    string="ExtraCondensed", nameID=16, platformID=3, platEncID=1, langID=0x409
 )
-ttfont["OS/2"].usWeightClass = 400
+ttfont["name"].setName(
+    string="Thin Italic", nameID=17, platformID=3, platEncID=1, langID=0x409
+)
+ttfont["OS/2"].usWeightClass = 100
 ttfont.save(font_path)
